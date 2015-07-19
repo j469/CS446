efded1021e44d7bba964f8db475c677dc87eb6b9
Jiayi Wang <j469wang@uwaterloo.ca>
2015-07-19 14:49:40 -0400

    For merge #21

--- FitGooseDemo/app/src/main/java/com/fitgoose/fitgoosedemo/plan_tab/ExerciseDetailsCard.java
+++ FitGooseDemo/app/src/main/java/com/fitgoose/fitgoosedemo/plan_tab/ExerciseDetailsCard.java
@@ -1,9 +1,12 @@
 package com.fitgoose.fitgoosedemo.plan_tab;
 
 import android.content.Context;
+import android.telephony.gsm.GsmCellLocation;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -39,11 +42,31 @@ public class ExerciseDetailsCard extends Card{
     public void setupInnerViewElements(ViewGroup parent, View view) {
 
         //Retrieve elements
-        TextView unit = (TextView) parent.findViewById(R.id.exercise_card_unit);
+        TextView unit_one = (TextView) parent.findViewById(R.id.exercise_card_unit_one);
+        unit_one.setText(GlobalVariables.exerciseUnit.get(exercise.unit1));
 
+        TextView unit_two = (TextView) parent.findViewById(R.id.exercise_card_unit_two);
+        TextView unit_two_name = (TextView) parent.findViewById(R.id.exercise_card_unit_two_name);
+        if (! exercise.secondUnit) {
+            unit_two_name.setVisibility(View.INVISIBLE);
+            unit_two.setVisibility(View.INVISIBLE);
+        } else {
+            unit_two_name.setVisibility(View.VISIBLE);
+            unit_two_name.setText("Unit 2:");
+            unit_two.setVisibility(View.VISIBLE);
+            unit_two.setText(GlobalVariables.exerciseUnit.get(exercise.unit2));
+        }
 
-        if (unit != null)
-            unit.setText("test.");
+        ((CheckBox) parent.findViewById(R.id.checkBox_exercise_shoulder)).setChecked(exercise.shoulder);
+        ((CheckBox) parent.findViewById(R.id.checkBox_exercise_chest)).setChecked(exercise.chest);
+        ((CheckBox) parent.findViewById(R.id.checkBox_exercise_farm)).setChecked(exercise.forearm);
+        ((CheckBox) parent.findViewById(R.id.checkBox_exercise_uarm)).setChecked(exercise.upper_arm);
+        ((CheckBox) parent.findViewById(R.id.checkBox_exercise_abs)).setChecked(exercise.abs);
+        ((CheckBox) parent.findViewById(R.id.checkBox_exercise_quads)).setChecked(exercise.quads);
+        ((CheckBox) parent.findViewById(R.id.checkBox_exercise_back)).setChecked(exercise.back);
+        ((CheckBox) parent.findViewById(R.id.checkBox_exercise_cardio)).setChecked(exercise.cardio);
+        ((CheckBox) parent.findViewById(R.id.checkBox_exercise_calves)).setChecked(exercise.calves);
+        ((CheckBox) parent.findViewById(R.id.checkBox_exercise_secondUnit)).setChecked(exercise.secondUnit);
 
     }
 
